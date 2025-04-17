5d1e96dc72dc3177cf0b96da1bdc80228521a662
Karl Bonde Torp <k.torp@samsung.com>
2025-04-17 14:08:50 +0200

    feat(libxnvme): add GPU kernel API
    Signed-off-by: Karl Bonde Torp <k.torp@samsung.com>

---
+++ include/libxnvme_kernels.h
@@ -0,0 +1,12 @@
+/**
+ * SPDX-FileCopyrightText: Samsung Electronics Co., Ltd
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * @headerfile libxnvme_kernels.h
+ */
+
+int
+xnvme_kernels_cmd_submit(uint32_t grid_size, uint32_t tblock_size, struct xnvme_dev *dev,
+			 uint32_t opc, uint32_t slba, uint32_t elba, uint32_t nlb, uint64_t nbytes,
+			 void *dbuf);
