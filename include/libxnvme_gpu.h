84c53549f695738a21eaa435f8f9473d227f8c8d
Karl Bonde Torp <k.torp@samsung.com>
2025-05-19 15:48:54 +0200

    feat(gpu): add range submit
    Signed-off-by: Karl Bonde Torp <k.torp@samsung.com>

--- include/libxnvme_gpu.h
+++ include/libxnvme_gpu.h
@@ -9,3 +9,7 @@
 int
 xnvme_gpu_cmd_submit(uint32_t grid_size, uint32_t tblock_size, struct xnvme_dev *dev, uint32_t opc,
 		     uint64_t slba, uint64_t elba, uint32_t nlb, uint64_t nbytes, void *dbuf);
+int
+xnvme_gpu_range_submit(uint32_t grid_size, uint32_t tblock_size, struct xnvme_dev *dev,
+		       uint32_t opc, uint64_t *slbas, uint64_t *elbas, uint32_t nlb,
+		       uint64_t nbytes, void **dbufs, uint32_t n_ranges);
