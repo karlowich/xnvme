ad15791dca54580f29c48144262768e1501a7fca
Karl Bonde Torp <k.torp@samsung.com>
2025-06-03 15:04:14 +0200

    WIP
    Signed-off-by: Karl Bonde Torp <k.torp@samsung.com>

--- lib/xnvme_kernels.cu
+++ lib/xnvme_kernels.cu
@@ -82,11 +82,48 @@ _range_submit(struct xnvme_dev *dev, uint32_t opc, uint32_t *slbas, uint32_t *el
 	}
 }
 
+
+__global__ void
+_range_submit2(struct xnvme_dev *dev, uint32_t opc, uint32_t *slbas, uint32_t *elbas, uint32_t nlb, uint64_t nbytes, void **dbufs, uint32_t n_ranges, uint32_t n_threads, uint32_t range)
+{
+	void *buf;
+	uint32_t slba = slbas[range];
+	uint32_t	elba = elbas[range];
+	void *	dbuf = dbufs[range];
+	uint32_t cur_slba;
+	uint64_t tid;
+	uint64_t i = 0;
+	while(true) {
+		struct xnvme_cmd_ctx ctx = {.dev = dev, .opts = XNVME_CMD_SYNC};
+		tid = blockIdx.x * blockDim.x + threadIdx.x + i*n_threads;
+
+		cur_slba = slba + tid * (nlb + 1);
+
+		if (cur_slba + nlb > elba) { // continue until reaching elba
+			return;
+		}
+
+		ctx.cmd.common.nsid = dev->ident.nsid;
+		ctx.cmd.common.opcode = opc;
+		ctx.cmd.nvm.nlb = nlb;
+		ctx.cmd.nvm.slba = cur_slba;
+
+		buf = (char *) dbuf + (cur_slba - slba) * dev->geo.lba_nbytes;
+
+		xnvme_be_bam_sync_cmd_io(&ctx, buf, nbytes, NULL, 0);
+		i++;
+	}
+}
+
 int
 xnvme_kernels_range_submit(uint32_t grid_size, uint32_t tblock_size, struct xnvme_dev *dev, uint32_t opc, uint32_t *slbas, uint32_t *elbas, uint32_t nlb, uint64_t nbytes, void **dbufs, uint32_t n_ranges)
 {
 	cudaError_t err;
-	_range_submit<<<grid_size, tblock_size>>>(dev, opc, slbas, elbas, nlb, nbytes, dbufs, n_ranges, grid_size * tblock_size);
+	// 	_range_submit<<<grid_size, tblock_size>>>(dev, opc, slbas, elbas, nlb, nbytes, dbufs, n_ranges, grid_size * tblock_size);
+	uint32_t gs = grid_size / n_ranges;
+	for (uint32_t i = 0; i < n_ranges; i++) {
+		_range_submit2<<<gs, tblock_size>>>(dev, opc, slbas, elbas, nlb, nbytes, dbufs, n_ranges, gs * tblock_size, i);
+	}
 	err = cudaGetLastError();
 	if (err != cudaSuccess) {
 		XNVME_DEBUG("Error launching kernel: %s", cudaGetErrorString(err));
